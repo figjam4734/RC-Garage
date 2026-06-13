3650f242ab95b437d96c42152bf08ca81109fb3d
figjam4734 <87471720+figjam4734@users.noreply.github.com>
2026-06-13 15:07:18 +0800

    Main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,1 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RCGarage from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RCGarage />
+  </React.StrictMode>,
+)
 
