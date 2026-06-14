50df6482d170e201f8a7ddad45133a513c91f950
figjam4734 <87471720+figjam4734@users.noreply.github.com>
2026-06-14 11:44:58 +0800

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,1 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = 'https://wgzgbnbdysypnrbihxsc.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndnemdibmJkeXN5cG5yYmloeHNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzMTQ0NTgsImV4cCI6MjA5Njg5MDQ1OH0.xGtRUA6jH-agrqLzSxHCKWgTcAnp1_jmjL0IM-m3jGU'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
